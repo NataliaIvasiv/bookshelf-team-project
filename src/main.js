a610a721074dc726b2e0d51c2d93fc61c6323d7a
NataliaIvasiv <143248403+NataliaIvasiv@users.noreply.github.com>
2024-02-28 23:23:39 +0200

    add libraries

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+import iziToast from "izitoast";
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
