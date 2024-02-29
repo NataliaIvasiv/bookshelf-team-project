8987b25c86de1738b0dd60856065a599c854ce7f
NataliaIvasiv <143248403+NataliaIvasiv@users.noreply.github.com>
2024-02-29 14:04:25 +0200

    add js-files, reset-colors-fonts

--- src/main.js
+++ src/main.js
@@ -7,6 +7,15 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 import { booksAPI } from "./js/booksAPI";
+import './js/category-render-function';
+import './js/category-template';
+import './js/localStorageFunctions';
+import './js/modal';
+import './js/refs';
+import './js/themes';
+import './js/shopping-list-render-function';
+import './js/shopping-list-template';
+
 
 const booksApi = new booksAPI();
 
