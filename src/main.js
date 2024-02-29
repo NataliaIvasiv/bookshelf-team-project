c003f72306d4d13a4a4bc0af8d46f49f1e1486ec
Chercheuse <108093435+Chercheuse@users.noreply.github.com>
2024-02-29 13:56:40 +0400

    delete import theme.js from main.js file

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import './js/themes.js';
 
 import { booksAPI } from "./js/booksAPI";
 
