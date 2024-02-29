07ef5824aa18c1ce4170f90bfd4d6dc3a6026951
kryvenkosasha <143883449+kryvenkosasha@users.noreply.github.com>
2024-02-29 22:33:32 +0100

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,15 +1,15 @@
-export function createModalMarkup(FROM_SERVER) {
+export function createModalMarkup(FROM_SERVER, FROM_LS) {
   const markup = `<div class="modal-background">
     <div class="modal-content-container">
     <button type="button" class="modal-close-btn"><img src="" class="modal-close-btn"/></button>
-    <img src="${FROM_SERVER}" class="modal-book-image" />
+    <img src="${FROM_SERVER.book_image}" class="modal-book-image" />
     <div class="modal-book-info">
-    <h3 class="modal-title">${FROM_SERVER}</h3>
-    <p class="modal-author">${FROM_SERVER}</p>
-    <p class="modal-description">${FROM_SERVER}</p>
+    <h3 class="modal-title">${FROM_SERVER.title}</h3>
+    <p class="modal-author">${FROM_SERVER.author}</p>
+    <p class="modal-description">${FROM_SERVER.description}</p>
     <ul class="modal-shops-list">
-    <li class="modal-shops-item"></li>
-    <li class="modal-shops-item"></li>
+    <li class="modal-shops-item"><a href="${FROM_SERVER.buy_links[0].url}"><img src="AMAZON"/></a></li>
+    <li class="modal-shops-item"><a href="${FROM_SERVER.buy_links[1].url}"><img src="APPLE BOOKS"/></a></li>
     </ul>
     <button type="button" class="modal-btn">${FROM_LS}</button>
     </div>
