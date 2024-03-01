cce2f7569fd2b87c06d4ac76c6f85c11b94a57c4
IrynaLebedenko <94801312+IrynaLebedenko@users.noreply.github.com>
2024-03-01 17:28:40 +0200

    add hover to book-image

--- src/js/category-render-function.js
+++ src/js/category-render-function.js
@@ -2,12 +2,17 @@ function getBookDetailInfo(selectedBookID){
     const { book_image, title, author } = image;
        
        return `<li class="book-item">
-                   <img class="book-card" src="${book_image}" alt="${title}">
-               <div class="book-details">
-                    <p class="book-title">${title}</p>
-                    <p class="book-author">${author}</p>
-               </div>
-           </li>`;
+                    <div class="book-item-container">
+                        <div class="book-card-container"> 
+                         <img class="book-card" src="${book_image}" alt="${title}">
+                            <p class="book-card-hover-text">QUICK VIEW</p>
+                        </div>
+                        <div class="book-details">
+                            <p class="book-title">${title}</p>
+                            <p class="book-author">${author}</p>
+                        </div>
+                    </div>
+              </li>`;
            } 
            
        
