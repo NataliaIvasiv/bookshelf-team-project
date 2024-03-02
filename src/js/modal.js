8b45bad96f7cbad1828eb178099658a6a6a4f255
kryvenkosasha <143883449+kryvenkosasha@users.noreply.github.com>
2024-03-02 11:27:33 +0100

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,7 +1,7 @@
 export function createModalMarkup(FROM_SERVER) {
-  const markup = `<div class="modal-background">
+  const markup = `<div class="modal-backdrop">
     <div class="modal-content-container">
-    <button type="button" class="modal-close-btn"><img src="" class="modal-close-btn"/></button>
+    <button type="submit" class="modal-close-btn"><img src="../img/symbol-defs.svg#icon-x-close" class="modal-close-btn"/></button>
     <img src="${FROM_SERVER.book_image}" class="modal-book-image" />
     <div class="modal-book-info">
     <h3 class="modal-title">${FROM_SERVER.title}</h3>
@@ -20,4 +20,44 @@ export function createModalMarkup(FROM_SERVER) {
 
 export function pushMarkup(markup) {
   document.body.insertAdjacentHTML('beforebegin', markup);
+}
+
+export function hideModal() {
+  const modalBackdrop = document.querySelector('.modal-backdrop');
+  const modalCloseBtn = document.querySelector('.modal-close-btn');
+
+  document.addEventListener('keydown', event => {
+    event.preventDefault();
+    if (event.key === 'Escape') {
+      removeListeners();
+    }
+  });
+
+  modalCloseBtn.addEventListener('click', event => {
+    event.preventDefault();
+    removeListeners();
+  });
+
+  modalBackdrop.addEventListener('click', event => {
+    event.preventDefault();
+    if (event.target === modalBackdrop) {
+      removeListeners();
+    }
+  });
+
+  function removeListeners() {
+    const modalBackdrop = document.querySelector('.modal-backdrop');
+
+    modalBackdrop.parentNode.removeChild(modalBackdrop);
+
+    document.removeEventListener('keydown', e => {
+      console.log('listener is remove');
+    });
+    modalCloseBtn.removeEventListener('click', e => {
+      console.log('listener is remove');
+    });
+    modalBackdrop.removeEventListener('click', e => {
+      console.log('listener is remove');
+    });
+  }
 }
