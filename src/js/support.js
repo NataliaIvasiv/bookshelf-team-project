3613c88c5e1d37e3f3c8635922432dc6dd39c572
Oksana2020LA <oksana2020la@gmail.com>
2024-03-02 07:10:54 -0800

    svg

--- src/js/support.js
+++ src/js/support.js
@@ -1,56 +1,39 @@
-export const supportArray = [
-  {
-    title: 'Save the Children',
-    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: require('../../img/support/support-img1@1x.png'),
-    img2: require('../../img/support/support-img1@2x.png'),
+import { supportArray } from './js/support-array';
+import { markupCardFund } from './js/support-array';
+import Swiper from 'swiper';
+
+const supportListEl = document.querySelector('.support__list-js');
+const btnSwiperEl = document.querySelector('.swiper-button-next');
+
+let position = 0;
+
+const addLeadingZero = value => {
+  return String(value).padStart(2, '0');
+};
+
+const markupSetFunds = fundArray
+  .map((el, i) => {
+    position = addLeadingZero(i + 1);
+
+    return markupCardFund(el, position);
+  })
+  .join('');
+
+supportListEl.innerHTML = markupSetFunds;
+
+const swiper = new Swiper('.swiper', {
+  direction: 'vertical',
+  spaceBetween: 20,
+  slidesPerView: 'auto',
+  rewind: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
   },
-  {
-    title: 'Project HOPE',
-    url: 'https://www.projecthope.org/country/ukraine/',
-    img: require('../../img/support/support-img2@1x.png'),
-    img2: require('../../img/support/support-img2@2x.png'),
-  },
-    {
-    title: 'International Medical Corps',
-    url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: require('../../img/support/support-img3@1x.png'),
-    img2: require('../../img/support/support-img3@2x.png'),
-  },
-  {
-    title: 'RAZOM',
-    url: 'https://www.razomforukraine.org/',
-    img: require('../../img/support/support-img4@1x.png'),
-    img2: require('../../img/support/support-img4@2x.png'),
-  },
-  {
-    title: 'Action against hunger',
-    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: require('../../img/support/support-img5@1x.png'),
-    img2: require('../../img/support/support-img5@2x.png'),
-  },
-  {
-    title: 'Serhiy Prytula Charity Foundation',
-    url: 'https://prytulafoundation.org/en',
-    img: require('../../img/support/support-img6@1x.png'),
-    img2: require('../../img/support/support-img6@2x.png'),
-  },
-  {
-    title: 'Medicins Sans Frontieres',
-    url: 'https://www.msf.org/ukraine',
-    img: require('../../img/support/support-img7@1x.png'),
-    img2: require('../../img/support/support-img7@2x.png'),
-  },
-  {
-    title: 'World vision',
-    url: 'https://www.wvi.org/emergencies/ukraine',
-    img: require('../../img/support/support-img8@1x.png'),
-    img2: require('../../img/support/support-img8@2x.png'),
-  }, 
-  {
-    title: 'UNITED24',
-    url: 'https://u24.gov.ua/uk',
-    img: require('../../img/support/support-img9@1x.png'),
-    img2: require('../../img/support/support-img9@2x.png'),
-  },
-];
+});
+
+swiper.update();
+
+btnSwiperEl.addEventListener('click', () => {
+  swiper.slideNext();
+});
