1ce7f56b7a42a6ed3bd4e9f34cae4b04b3384a13
RomanaKarasenko <143837231+RomanaKarasenko@users.noreply.github.com>
2024-03-02 17:27:29 +0200

    Updated-SL-render

--- src/js/shopping-list-render-function.js
+++ src/js/shopping-list-render-function.js
@@ -1,3 +1,5 @@
+const shoppingListContainer = document.querySelector('.shopping-list');
+
 export function createShoppingListMarkup(arrayOfBooks) {
   let result;
   if (arrayOfBooks.length === 0) {
@@ -5,10 +7,8 @@ export function createShoppingListMarkup(arrayOfBooks) {
   } else {
     const bookItems = arrayOfBooks.map(createBookMarkup).join('');
     result = `
-    <div class="container  shopping-list-container">
-      <h2 class="shopping-list-title">Shopping <span class="shopging-list-title-span">List</span></h2>
-      <ul class="shopping-list">${bookItems}</ul>
-    </div>`;
+      ${bookItems}
+    `;
   }
   return result;
 }
@@ -16,10 +16,14 @@ export function createShoppingListMarkup(arrayOfBooks) {
 export function createEmptyBackground() {
   return `
       <div class="container  shopping-list-container">
-        <h2 class="shopping-list-title">Shopping <span class="shopping-list-title-span">List</span></h2>
         <div class="shopping-list-empty-container">
           <p class="shopping-list-empty-paragraph">This page is empty, add some books and proceed to order.</p>
           <img class="shopping-list-empty-img" src="" alt="Books in empty section"/>
         </div>
       </div>`;
 }
+
+shoppingListContainer.insertAdjacentHTML(
+  'beforeend',
+  createShoppingListMarkup([])
+);
