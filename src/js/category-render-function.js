ee7754c1cadc6a4d76b51c187d21003b5590eb67
IrynaLebedenko <94801312+IrynaLebedenko@users.noreply.github.com>
2024-03-04 18:59:37 +0200

    add styles to title-book-section

--- src/js/category-render-function.js
+++ src/js/category-render-function.js
@@ -11,3 +11,14 @@ export function bookTitle(selectedCategory) {
   const markupBookTitle = `${selectedCategory.textContent}`;
   refs.categoriesMainTitle.insertAdjacentHTML('beforeend', markupBookTitle)
 }
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const elements = document.querySelectorAll('.title-book-section');
+  elements.forEach(function(element) {
+    const text = element.textContent.trim();
+    const lastWordIndex = text.lastIndexOf(' ');
+    const html = text.slice(0, lastWordIndex) + '<span class="last-word">' + text.slice(lastWordIndex) + '</span>';
+    element.innerHTML = html;
+  });
+});
