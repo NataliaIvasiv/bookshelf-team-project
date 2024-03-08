e8d6bb29784b6020ab67fc942900f6d5327fe71f
NataliaIvasiv <143248403+NataliaIvasiv@users.noreply.github.com>
2024-03-08 12:58:31 +0000

    Deploying to gh-pages from @ NataliaIvasiv/bookshelf-team-project@072af54bc251c3160a4cf7f846fa420d5d3fb169 🚀

--- assets/support-95c88a2d.js
+++ assets/support-95c88a2d.js
@@ -52,4 +52,4 @@ import{i as I,S as f}from"./vendor-e2ce0d38.js";(function(){const s=document.cre
            <img class="support-img" src="${i}" data-source="${t}" alt="${e}" />
        </a>
      </div>`}).join("");lA(pA);const dA=new f(".swiper",{direction:"vertical",loop:!0,slidesPerView:4,spaceBetween:20,breakpoints:{768:{slidesPerView:6,spaceBetween:10}}});function uA(){dA.slideNext(200)}export{l as a,R as b,y as c,p as d,S as e,k as f,A as r};
-//# sourceMappingURL=support-12fb6444.js.map
+//# sourceMappingURL=support-95c88a2d.js.map
